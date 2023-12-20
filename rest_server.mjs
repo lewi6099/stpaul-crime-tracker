7e36cf15229fc85873cda2d02e045ba2b63fe2f3
ethanjohnson <john64862@stthomas.edu>
2023-12-19 19:24:51 -0600

    Fixed issue with Access-Control-Allow-Origin

--- rest_server.mjs
+++ rest_server.mjs
@@ -15,9 +15,17 @@ const corsOptions = {
     origin: "http://localhost:5173",
 };
 
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 let app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(allowCrossDomain);
 
 /********************************************************************
  ***   DATABASE FUNCTIONS                                         *** 
